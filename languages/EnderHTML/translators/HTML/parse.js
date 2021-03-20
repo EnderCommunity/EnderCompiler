778df14718d8ea30d3c3f5d5026de58e997e2cde
adel-sbeh <72353925+adel-sbeh@users.noreply.github.com>
2021-03-20 23:24:32 +0200

    Added a test

---
+++ languages/EnderHTML/translators/HTML/parse.js
@@ -0,0 +1,24 @@
+//The `Translator` Object is already defined! The only value it has is the current work-dir!
+//You can add some functions here that have something to do with the object.
+
+const parse = require('html-dom-parser');
+
+module.exports = function(code) {
+    var compiledCode = "",
+        _this = this;
+    code = code.toString().split(/(<component:.*\/>)/);
+    code.forEach(function(c) {
+        if (c.replace(/\s/g, "").replace(/\n/g, "").indexOf("<component:") >= 0) {
+            c = parse(c)[0];
+            c.name = c.name.replace(/component:/g, "");
+            if (_this.components[c.name] !== undefined) {
+                compiledCode += _this.components[c.name](c.attribs);
+            } else {
+                throw new Error("An unknown component was found!");
+            }
+        } else
+            compiledCode += c;
+    });
+    return compiledCode;
+};
+//Don't add any other functions that are not necessary!
